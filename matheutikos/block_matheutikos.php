f0b1d7223e245fb9ddc195ef87f0aa7b7a7f5cb7
Aitor Pérez <elinsidiomate@gmail.com>
2015-07-31 11:35:06 +0200

    versió final

--- matheutikos/block_matheutikos.php
+++ matheutikos/block_matheutikos.php
@@ -12,11 +12,13 @@ class block_matheutikos extends block_base {
 
     global $DB, $USER;
 
-    $credential = $DB->get_field("rcommon_user_credentials", 'credentials', array('euserid' => $USER->id, 'isbn' => "matheutikos4"));
+    $isbns = $DB->get_fieldset_select("rcommon_user_credentials", 'isbn', 'euserid = ?', array($USER->id));
+    $isbn = max($isbns);    
+    $credential = $DB->get_field("rcommon_user_credentials", 'credentials', array('euserid' => $USER->id, 'isbn' => $isbn), IGNORE_MULTIPLE);
 
     $this->content = new stdClass;
-    $this->content->text = file_get_contents("http://matheutikos.dev/moodle/block/$credential");
-    $this->content->footer = '<a href="http://matheutikos.dev/moodle/access/' . $credential . '" target="_blank">' . get_string('link', 'block_matheutikos') . '</a>';
+    $this->content->text = file_get_contents("http://school.matheutikos.com/moodle/block/$credential");
+    $this->content->footer = '<a href="http://school.matheutikos.com/moodle/access/' . $credential . '" target="_blank">' . get_string('link', 'block_matheutikos') . '</a>';
 
     return $this->content;
   }
